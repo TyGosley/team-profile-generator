a30ffd414ca8ad76e51be7f5c76fbb7c67f803b5
TyGosley <tygosley@gmail.com>
2023-02-27 16:53:25 -0800

    styled fonts CSS and link generateHTML

--- Main/lib/generateHTML.js
+++ Main/lib/generateHTML.js
@@ -13,15 +13,16 @@ const createTeamPage = function (employeeCards) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&family=Tilt+Warp&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../src/style.css">
 
     <title>Team Profile</title>
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-              <span class="navbar-brand mb-0 h1">Team Assemble</span>
+        <nav class="navbar">
+            <div class="container">
+              <span class="navbar-brand">Team Profile...Assemble</span>
             </div>
           </nav>
     </header>
